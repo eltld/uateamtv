479fbc72927369c0cda3e0a109dc7124c1ddaf93
TarasMazepa <mazepa.t@gmail.com>
2014-11-16 14:45:41 +0200

    fixes #29 Remove useless series items

--- app/src/main/java/com/github/tarasmazepa/uateam/uateamtv/fragment/base/LinkListFragment.java
+++ app/src/main/java/com/github/tarasmazepa/uateam/uateamtv/fragment/base/LinkListFragment.java
@@ -3,6 +3,7 @@ package com.github.tarasmazepa.uateam.uateamtv.fragment.base;
 import com.github.tarasmazepa.uateam.uateamtv.adapter.ViewFiller;
 import com.github.tarasmazepa.uateam.uateamtv.model.Link;
 import com.google.common.base.Function;
+import com.google.common.base.Predicate;
 import com.google.common.collect.Collections2;
 import com.google.common.collect.Lists;
 
@@ -12,17 +13,24 @@ import java.util.Collection;
 import java.util.List;
 
 public abstract class LinkListFragment extends ListFragment<Link> {
+    private static final Predicate<Link> NOT_SERIES_BUNDLE_LINK = new Predicate<Link>() {
+        @Override
+        public boolean apply(Link input) {
+            return !input.title.matches("Повний \\d сезон");
+        }
+    };
+
     @Override
     protected ViewFiller<Link> getViewFiller() {
         return new ViewFiller<Link>();
     }
 
     public List<Link> transformToLinks(Collection<Element> elements) {
-        return Lists.newArrayList(Collections2.transform(elements, new Function<Element, Link>() {
+        return Lists.newArrayList(Collections2.filter(Collections2.transform(elements, new Function<Element, Link>() {
             @Override
             public Link apply(Element input) {
                 return new Link(input.text(), input.attr("href"));
             }
-        }));
+        }), NOT_SERIES_BUNDLE_LINK));
     }
 }
